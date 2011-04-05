5a63e3649a2c09b4bf726615e164cfe4bb3f17e2
Vijay Ramane <vkr0704@rediffmail.com>
2011-04-05 14:32:55 +0530

    Initial commit

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
 def index
 	@q=params[:q]
-  @name = Frnd.find(:all, :conditions=>{:name=>@q})
+  @name = Frnd.where("name=:qa OR age=:qa AND age!=0", {:qa => params[:q]})
 	end
 
 def show
